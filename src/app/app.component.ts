ca85c4b8ddaf387288e8c8be6810530fa36dd2d2
Mary Denysova <djailin.vasu@gmail.com>
2020-04-09 23:28:11 +0300

    Создание формы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular Forms';
+
+  form: FormGroup
+
+  ngOnInit() {
+    this.form = new FormGroup({})
+  }
+
+  submit() {
+
+  }
 }
